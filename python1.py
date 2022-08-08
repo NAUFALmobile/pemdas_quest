09b6ee485a6e1754f5d540411426353f81265066
NAUFALmobile <110508991+NAUFALmobile@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Helloo World")
+print("Nama Saya Naufal")
+print("Saya Penyuka Alam")
+print("Bau Alam")
+print("Alam Yang Indah")
+print("Menjadi Ovt Memang Tidak Enak")
+print("Digosting Sakit Gak Sih")
+print("Cape Belum Disemangatin Ayang")
+print("Udah Sayang Tapi Malah Di Abay Kan Hati Nya")
+print("Cukup Stay Di Hati Hawra Aja")  
---
+++ python2.py
@@ -0,0 +1,12 @@
+# Belajar Input
+
+nama = "naufal"
+umur = 15
+alamat = "komp pelni, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama)
+
