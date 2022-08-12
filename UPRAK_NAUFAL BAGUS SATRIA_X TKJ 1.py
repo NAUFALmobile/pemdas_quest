fb72714fa177d1591ec9d4c97118148e4bc07055
NAUFALmobile <110508991+NAUFALmobile@users.noreply.github.com>
2022-08-11 20:54:25 -0700

    Add files via upload

---
+++ UPRAK_NAUFAL BAGUS SATRIA_X TKJ 1.py
@@ -0,0 +1,8 @@
+nama_depan = input("Masukkan Nama Depan Anda: ")
+nama = input("Masukkan Nama Belakang Anda: ")
+
+print("Nama Anda Adalah", nama_depan, nama)
+
+celcius = int(input("Masukkan Suhu Celcius: "))
+reamur = (4/5 * celcius)
+print(celcius, "Celcius Adalah", reamur, "reamur")
